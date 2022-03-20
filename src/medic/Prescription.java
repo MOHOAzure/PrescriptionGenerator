f0e8fd2e05f3a500a59bdbcd7377e2411cbf961c
MOHOAzure <timecrutramer@gmail.com>
2022-03-20 08:36:37 +0800

    Update Prescription.java

--- src/medic/Prescription.java
+++ src/medic/Prescription.java
@@ -1,62 +1,62 @@
 package medic;
 
 public class Prescription {
-	private String houseName;
-	private String residentName;
-	private String departmentName;
-	private int progress;
-	//private String doctorName;
-	
-	public Prescription(){
-		super();
-	}
-	
-	public String toString() {
-        return "Employee ["+ 
-	        		"houseName=" + houseName + 
-	        		", residentName=" + residentName + 
-	        		", departmentName=" + departmentName + 
-	        		", progress=" + progress +
-	        		//", doctorName=" + doctorName +
-                "]";
-    }
-	public Prescription(String houseName, String residentName, String departmentName, int progress){
-		super();
-		this.setHouseName(houseName);
-		this.setResidentName(residentName);
-		this.setDepartmentName(departmentName);
-		this.setProgress(progress);
-	}
+  private String houseName;
+  private String residentName;
+  private String departmentName;
+  private int progress;
+  //private String doctorName;
 
-	public int getProgress() {
-		return progress;
-	}
+  public Prescription() {
+    super();
+  }
 
-	public void setProgress(int progress) {
-		this.progress = progress;
-	}
+  public String toString() {
+    return "Employee [" +
+      "houseName=" + houseName +
+      ", residentName=" + residentName +
+      ", departmentName=" + departmentName +
+      ", progress=" + progress +
+      //", doctorName=" + doctorName +
+      "]";
+  }
+  public Prescription(String houseName, String residentName, String departmentName, int progress) {
+    super();
+    this.setHouseName(houseName);
+    this.setResidentName(residentName);
+    this.setDepartmentName(departmentName);
+    this.setProgress(progress);
+  }
 
-	public String getHouseName() {
-		return houseName;
-	}
+  public int getProgress() {
+    return progress;
+  }
 
-	public void setHouseName(String houseName) {
-		this.houseName = houseName;
-	}
+  public void setProgress(int progress) {
+    this.progress = progress;
+  }
 
-	public String getResidentName() {
-		return residentName;
-	}
+  public String getHouseName() {
+    return houseName;
+  }
 
-	public void setResidentName(String residentName) {
-		this.residentName = residentName;
-	}
+  public void setHouseName(String houseName) {
+    this.houseName = houseName;
+  }
 
-	public String getDepartmentName() {
-		return departmentName;
-	}
+  public String getResidentName() {
+    return residentName;
+  }
 
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
+  public void setResidentName(String residentName) {
+    this.residentName = residentName;
+  }
+
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
 }
